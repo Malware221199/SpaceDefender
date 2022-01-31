75b477ba00cd77d0f30192744c2f0922cbcb3bc0
Malware221199 <samuele.starita@gmail.com>
2022-01-31 20:17:24 +0100

    s

--- Progetto Uni/Alieni.c
+++ Progetto Uni/Alieni.c
@@ -11,7 +11,27 @@ void Alieni(int pipeout, int contatore)
 struct position Alieni;
 int deltax=1;		/* Spostamento orizzontale */
 int deltay=1;		/* Spostamento verticale */
-  if(contatore==0){
+  //if(contatore==0){
+  //Alieni.x = MAXX-2;  /* Coordinata X iniziale */
+  //Alieni.y = 1;  /* Coordinata Y iniziale */
+  //Alieni.c ='A';	/* Carattere identificativo */
+  //printf("hallo");
+  //}
+  //else if(contatore==1){
+  //Alieni.x = MAXX-2;  /* Coordinata X iniziale */
+  //Alieni.y = 1;  /* Coordinata Y iniziale */
+  //Alieni.c ='A';	/* Carattere identificativo */
+  //printf("ciao");
+  //}
+   /* Comunico le coordinate iniziali al processo padre */
+  //write(pipeout,&Alieni,sizeof(Alieni));
+
+  while(1){
+
+    /* Movimento Y */
+    //Alieni.y += deltay;
+
+    if(contatore==0){
   Alieni.x = MAXX-2;  /* Coordinata X iniziale */
   Alieni.y = 1;  /* Coordinata Y iniziale */
   Alieni.c ='A';	/* Carattere identificativo */
@@ -23,15 +43,6 @@ int deltay=1;		/* Spostamento verticale */
   Alieni.c ='A';	/* Carattere identificativo */
   printf("ciao");
   }
-   /* Comunico le coordinate iniziali al processo padre */
-  write(pipeout,&Alieni,sizeof(Alieni));
-
-  while(1){
-
-    /* Movimento Y */
-    //Alieni.y += deltay;
-
-
     /* Se supero area X schermo inverte il movimento */
     if(Alieni.x + deltax < 1 || Alieni.x + deltax > MAXX){
       Alieni.x = MAXX-2;  /* Coordinata X iniziale */
