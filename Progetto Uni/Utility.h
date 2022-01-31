3d68014b8c4bd29f33a1245259378225da0693b9
Malware221199 <samuele.starita@gmail.com>
2022-01-31 21:28:19 +0100

    s

--- Progetto Uni/Utility.h
+++ Progetto Uni/Utility.h
@@ -11,8 +11,8 @@
 #define DX			67		/* Cursore destra */
 #define SPC			32		/* Cursore spazio */
 #define ENTER		10	  /* Cursore invio */
-#define	MAXX		60		/* Dimensione dello schermo di output (colonne) */
-#define MAXY		20		/* Dimensione dello schermo di output (righe)   */
+#define	MAXX		80		/* Dimensione dello schermo di output (colonne) */
+#define MAXY		25		/* Dimensione dello schermo di output (righe)   */
 #define DELAYV 	120000	    /* Ritardo nel movimento delle navicelle nemiche veloce */
 #define DELAYM 	160000	    /* Ritardo nel movimento delle navicelle nemiche medio */
 #define DELAYL 	200000	    /* Ritardo nel movimento delle navicelle nemiche lento */
