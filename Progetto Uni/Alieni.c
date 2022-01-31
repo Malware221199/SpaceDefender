fe144b01432e54b6dcb2bf726e77b8a961da3f81
Malware221199 <samuele.starita@gmail.com>
2022-01-31 21:31:31 +0100

    s

--- Progetto Uni/Alieni.c
+++ Progetto Uni/Alieni.c
@@ -11,18 +11,11 @@ void Alieni(int pipeout, int contatore)
 struct position Alieni;
 int deltax=1;		/* Spostamento orizzontale */
 int deltay=1;		/* Spostamento verticale */
-  if(contatore==0){
+
   Alieni.x = MAXX-2;  /* Coordinata X iniziale */
   Alieni.y = MAXX/2;  /* Coordinata Y iniziale */
   Alieni.c ='A';	/* Carattere identificativo */
-  mvprintw(1,1,"contattore 0");
-  }
-  else if(contatore==1){
-  Alieni.x = MAXX-2;  /* Coordinata X iniziale */
-  Alieni.y = 5;  /* Coordinata Y iniziale */
-  Alieni.c ='A';	/* Carattere identificativo */
-  printf("contatore 1");
-  }
+  
    /* Comunico le coordinate iniziali al processo padre */
   write(pipeout,&Alieni,sizeof(Alieni));
 
