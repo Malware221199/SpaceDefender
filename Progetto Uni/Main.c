09226a12130814c4764b797f9922828091870d38
Malware221199 <samuele.starita@gmail.com>
2022-01-31 20:40:14 +0100

    s

--- Progetto Uni/Main.c
+++ Progetto Uni/Main.c
@@ -30,6 +30,7 @@ int main(){
         for(i=0;i<2;i++){
         /* Creo il primo processo figlio 'Alieni' */
         pidA = fork();
+        printf("Pid creato");
         }
         /* Se il pid == 0 -> si tratta del processo 'Alieni' */
         if(pidA==0) {
