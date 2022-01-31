bd597ebf5e7605787479b50eb2448369b07f3cb2
Malware221199 <samuele.starita@gmail.com>
2022-01-31 14:10:45 +0100

    s

--- Progetto Uni/Grafica.c
+++ Progetto Uni/Grafica.c
@@ -21,9 +21,10 @@ void stampasprite(int larghezza,int altezza,char entita){
 
 void cancellasprite(int larghezza,int altezza){
     int i,j;
-    for(i=0;i<larghezza;i++){
-        for(j=0;j<altezza;j++){
-            mvaddch(j,i,' ');
-        }
-    }
+    //for(i=0;i<larghezza;i++){
+        //for(j=0;j<altezza;j++){
+            //mvaddch(j,i,' ');
+        //}
+    //}
+    mvaddch(larghezza,altezza,' ');
 }
