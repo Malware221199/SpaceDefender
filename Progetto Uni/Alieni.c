a01451c8ac64a09f46e42292b14ec6288b650aa4
Malware221199 <samuele.starita@gmail.com>
2022-02-01 12:28:19 +0100

    s

--- Progetto Uni/Alieni.c
+++ Progetto Uni/Alieni.c
@@ -5,7 +5,7 @@
 ---------------------------------------------------------------------- 
 */
 void Gen_alieni(int pipeout,int nnemici){
-      //do{
+      do{
         int pidA;
         pidA = fork();
         mvprintw(0,1,"PID %d",pidA);
@@ -14,7 +14,7 @@ void Gen_alieni(int pipeout,int nnemici){
             Alieni(p[1],nnemici); /* invocazione funzione alieni */
             //nnemici--;   
         }
-      //}while(nnemici>0);
+      }while(nnemici>0);
 }
 
 /*
