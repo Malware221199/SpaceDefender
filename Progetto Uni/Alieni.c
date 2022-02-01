c5d2f670dc0f90e5320908acde68c18c8d1863f2
Malware221199 <samuele.starita@gmail.com>
2022-02-01 19:40:29 +0100

    s

--- Progetto Uni/Alieni.c
+++ Progetto Uni/Alieni.c
@@ -25,7 +25,7 @@ int deltay=1;		/* Spostamento verticale */
     /* Se supero area X schermo inverte il movimento */
     if(Alieni.x + deltax < 1 || Alieni.x + deltax > MAXX){
       Alieni.x = MAXX-2;  /* Coordinata X iniziale */
-      Alieni.y = MAXY/2;  /* Coordinata Y iniziale */
+      Alieni.y = (MAXY/2)+(5*id);  /* Coordinata Y iniziale */
       }
     
     /* Movimento X */
