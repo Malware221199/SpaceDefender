ec8c71cedecd7fdb8391d3e72572ebda6172edb3
Malware221199 <samuele.starita@gmail.com>
2022-02-01 14:42:35 +0100

    s

--- Progetto Uni/Grafica.c
+++ Progetto Uni/Grafica.c
@@ -3,6 +3,8 @@
 void stampasprite(int y,int x,char c,int n){
     if(c=='A'){
         int i;
+        y=y+n;
+        x=x+n;
         //usleep(3000000);
         mvprintw(0,6,"N %d",n);
         for(i=0;i<3;i++)
