356b78f547f4b7d2985b1119f7b269ce1880b322
Malware221199 <samuele.starita@gmail.com>
2022-02-02 17:51:01 +0100

    s

--- Progetto Uni/Grafica.c
+++ Progetto Uni/Grafica.c
@@ -22,7 +22,7 @@ void stampasprite(int y,int x,char c,int n){
     else if(c=='B'){
         /* Visualizzo il giocatore*/
             attron(COLOR_PAIR(4));
-            mvprintw(y,x,">");
+            mvprintw(y,x,"%d",n);
             attroff(COLOR_PAIR(4));
 
     }
