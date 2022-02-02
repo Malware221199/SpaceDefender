f910d354b3c6e81894e4e0b574037929155437d5
Malware221199 <samuele.starita@gmail.com>
2022-02-02 23:52:02 +0100

    s

--- Progetto Uni/Giocatore.c
+++ Progetto Uni/Giocatore.c
@@ -37,18 +37,16 @@ void giocatore(int pipeout)
       if(pidB1==0) {
         close(p[0]); /* chiusura del descrittore di lettura */
         bullet(p[1],idb,giocatore.y,giocatore.x); /* invocazione funzione bullet */
-      }
-      else {		
-        /* Altrimenti sono ancora nel processo padre e creo il processo 'Giocatore' */
-        pidB2=fork();
-                            
-        /* Se il pid == 0 -> si tratta del processo 'Giocatore' */
-        if(pidB2==0) {
-          /* ed eseguo quindi la relativa funzione di gestione */
-          close(p[0]); /* chiusura del descrittore di lettura */
-          bullet(p[1],idb,giocatore.y,giocatore.x); /* invocazione funzione bullet */ 
+      }	
+      /* Altrimenti sono ancora nel processo padre e creo il processo 'Giocatore' */
+      pidB2=fork();
+      /* Se il pid == 0 -> si tratta del processo 'Giocatore' */
+      if(pidB2==0) {
+        /* ed eseguo quindi la relativa funzione di gestione */
+        close(p[0]); /* chiusura del descrittore di lettura */
+        bullet(p[1],idb,giocatore.y,giocatore.x); /* invocazione funzione bullet */ 
         }
-      }
+      
     }
 
     /* Comunico al processo padre le coordinate del giocatore */
