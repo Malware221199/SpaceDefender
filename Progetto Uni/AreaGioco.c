4817810a32d998e95800112962700294d38ea559
Malware221199 <samuele.starita@gmail.com>
2022-02-02 22:04:24 +0100

    s

--- Progetto Uni/AreaGioco.c
+++ Progetto Uni/AreaGioco.c
@@ -72,13 +72,13 @@ void area(int pipein){
             
             /* Segnalo collisione e tipo*/
             int i,j,k;
-            /*for(i=0;i<11;i++){
+            for(i=0;i<11;i++){
                 for(j=0;j<3;j++){
                     for(k=0;k<3;k++){
                         if(bullet[dato_letto.id].x == (alieni[i].x)+k && bullet[dato_letto.id].y == (alieni[i].y)+j) vite--;
                     }
                 }
-            }*/
+            }
             
 
             /* Visualizzo le vite rimaste al contadino */
