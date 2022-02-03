fdfa08bb378dbc0eea6cd662e7e8187c276647d9
Malware221199 <samuele.starita@gmail.com>
2022-02-04 00:03:30 +0100

    s

--- Progetto Uni/Main.c
+++ Progetto Uni/Main.c
@@ -23,7 +23,7 @@ int main(){
         init_pair(4,COLOR_WHITE,COLOR_BLACK);   /* Colore trappola */
         attron(COLOR_PAIR(4));
         int ida;
-        int nnemici=2;
+        int nnemici=1;
         int difficolta = DELAYM;
         if ( menu(nnemici,difficolta) == 0){
             clear();
