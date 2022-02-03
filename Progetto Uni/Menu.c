8ce2167cead35ba1a3264d8a0fed6c7fad909603
Malware221199 <samuele.starita@gmail.com>
2022-02-03 22:38:58 +0100

    s

--- Progetto Uni/Menu.c
+++ Progetto Uni/Menu.c
@@ -21,13 +21,13 @@ int menu(int *nnemici,int *difficolta){
       if(i==0){
         if(i == altezza){
         attron(COLOR_PAIR(1));
-        stampasprite(MAXY-12,MAXX/2-13,'S',26);
+        stampasprite(MAXY-12,MAXX/2-8,'S');
         attroff(COLOR_PAIR(1));
         ;
         }
         else{
         attron(COLOR_PAIR(2));
-        stampasprite(MAXY-12,MAXX/2-13,'S',26);;
+        stampasprite(MAXY-12,MAXX/2-8,'S');
         attroff(COLOR_PAIR(2));
         }
       }
