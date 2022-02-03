58baaf6f8dc4305408668a21e1ca84a0212cbee2
Malware221199 <samuele.starita@gmail.com>
2022-02-03 15:02:17 +0100

    s

--- Progetto Uni/AreaGioco.c
+++ Progetto Uni/AreaGioco.c
@@ -77,7 +77,7 @@ void area(int pipein){
             for(i=0;i<11;i++){
                 for(j=0;j<3;j++){
                     for(k=0;k<3;k++){
-                        if(bullet[dato_letto.id].x == (alieni[i].x)+k && bullet[dato_letto.id].y == (alieni[i].y)+j) kill(pidA,1);;
+                        if(bullet[dato_letto.id].x == (alieni[i].x)+k && bullet[dato_letto.id].y == (alieni[i].y)+j) kill(pidA,alieni[i].pid);;
                     }
                 }
             }
