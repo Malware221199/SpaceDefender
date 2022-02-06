15bf92735a131b5a7b90bf75c48b3fc2d3f87d83
Malware221199 <samuele.starita@gmail.com>
2022-02-06 02:49:02 +0100

    s

--- Progetto Uni/Alieni.c
+++ Progetto Uni/Alieni.c
@@ -7,7 +7,7 @@
 
 void alieni(int pipeout, int id,int nnemici, int difficolta){
 struct position Alieni;
-int r=1,idbn=0;
+int idbn=0;
 int prova=0;
 int deltax=1;		/* Spostamento orizzontale */
 int deltay=1;		/* Spostamento verticale */
@@ -16,10 +16,7 @@ if(id==2||id==3) Alieni.x= MAXX-(GSA+DA)*2;
 if(id==4||id==5) Alieni.x= MAXX-(GSA+DA)*3;
 if(id==6||id==7) Alieni.x= MAXX-(GSA+DA)*4;
 if(id==8||id==9) Alieni.x= MAXX-(GSA+DA)*5;
-  if(id%2==0){
-    Alieni.y=(MAXY/4);
-    r++;
-  }
+  if(id%2==0) Alieni.y=(MAXY/4);
   else if (id%2==1) Alieni.y=MAXY-(MAXY/4)-3;
   
   Alieni.c ='A';	/* Carattere identificativo */
@@ -45,16 +42,28 @@ if(id==8||id==9) Alieni.x= MAXX-(GSA+DA)*5;
     
     /* Movimento X */
     Alieni.x -= deltax;
-
-    if (random() < RAND_MAX/10){
-      idbn++;
-      pidBN=fork();
-      if(pidBN==0) {
-      close(p[0]);
-      prova=Alieni.y;
-      bulletn(p[1],idbn,prova,Alieni.x);
+    if(id%2==0){
+      if (random() < RAND_MAX/10){
+        idbn++;
+        pidBN=fork();
+        if(pidBN==0) {
+        close(p[0]);
+        bulletn(p[1],idbn,MAXY/4,Alieni.x);
+        }
       }
     }
+
+    else if(id%2==1){
+      if (random() < RAND_MAX/10){
+        idbn++;
+        pidBN=fork();
+        if(pidBN==0) {
+        close(p[0]);
+        bulletn(p[1],idbn,MAXY-(MAXY/4)-3,Alieni.x);
+        }
+      }
+    }
+
     /* Comunico le coordinate correnti al processo padre */
     write(pipeout,&Alieni,sizeof(Alieni));
 
