ae6a3b5f4c1bf93feb8806753b493e20fcfc7319
Malware221199 <samuele.starita@gmail.com>
2022-06-07 16:57:39 +0200

    s

--- Space Defender thread/AreaGioco.c
+++ Space Defender thread/AreaGioco.c
@@ -252,7 +252,7 @@ void area(void){
             if(vite < 1) {
                 collision=1;
                 clear();
-                stampasprite(MAXY/2-2,MAXX/2-25,'O');
+                //stampasprite(MAXY/2-2,MAXX/2-25,'O');
                 refresh();
                 usleep(4000000);
                 pthread_join (tbulletg, NULL);
