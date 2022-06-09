8ff20ed334609c079b35c55ddea696da49df75c5
Malware221199 <samuele.starita@gmail.com>
2022-06-09 21:34:32 +0200

    s

--- Space Defender thread/AreaGioco.c
+++ Space Defender thread/AreaGioco.c
@@ -40,7 +40,25 @@ void *alieni(void *arg){
 
     while(!collision){
         pthread_mutex_lock(&malieni);
+        /* Movimento X */
         A[myida].cord.x-=1;
+        /* Movimento Y */
+        if(A[myida].cord.y%2==0){
+            if(A[myida].cord.y%2==0){
+            
+            A[myida].cord.y += deltay;
+            /* Se supero area Y schermo inverte il movimento */
+            if(A[myida].cord.y + deltay < 0 || A[myida].cord.y + deltay > MAXY/2) deltay = -deltay;
+           
+        }
+        else{
+            /* Movimento Y */
+            A[myida].cord.y -= deltay;
+            /* Se supero area Y schermo inverte il movimento */
+            if(A[myida].cord.y - deltay < MAXY/2 || A[myida].cord.y - deltay > MAXY) deltay = -deltay;
+            
+        }
+            
         pthread_mutex_unlock(&malieni);
 
         if (random() < RAND_MAX/10){
