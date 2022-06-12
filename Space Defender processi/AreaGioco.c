2c0b5d0122ead62535e7d7f60722f949a3660c2a
Malware221199 <samuele.starita@gmail.com>
2022-06-12 10:57:09 +0200

    s

--- Space Defender processi/AreaGioco.c
+++ Space Defender processi/AreaGioco.c
@@ -96,23 +96,23 @@ void area(int pipein,int nnemici){
                 }
                 /* Collisioni proiettili con alieni*/
                  for(i=0;i<11;i++){
-                    int savepid;
-                    savepid=A[i].id;
-                    if(A[savepid].alive){
+                    
+                    
+                    if(A[i].alive){
                        
-                        if(BG[dato_letto.id].alive && collisione(BG[dato_letto.id], DB, DB,A[savepid],GSA,GSA) && A[savepid].liv==1)
+                        if(BG[dato_letto.id].alive && collisione(BG[dato_letto.id], DB, DB,A[i],GSA,GSA) && A[i].liv==1)
                         {   
                             
                             BG[dato_letto.id].alive=false;
                             kill(BG[dato_letto.id].pid,1);
-                            A[savepid].liv=2;
+                            A[i].liv=2;
                         }
-                        else if(BG[dato_letto.id].alive && collisione(BG[dato_letto.id], DB, DB,A[savepid],GSA,GSA) && A[savepid].liv==2)
+                        else if(BG[dato_letto.id].alive && collisione(BG[dato_letto.id], DB, DB,A[i],GSA,GSA) && A[i].liv==2)
                         {
                             BG[dato_letto.id].alive=false;
                             kill(BG[dato_letto.id].pid,1);
-                            A[savepid].alive=false;
-                            kill(A[savepid].pid,1);
+                            A[i].alive=false;
+                            kill(A[i].pid,1);
                             alienimorti++;
                         }
                             
