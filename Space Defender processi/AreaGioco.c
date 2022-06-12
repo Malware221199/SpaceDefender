71a19ed4a9856a53a85a63c400cd481cf5cd4b18
Malware221199 <samuele.starita@gmail.com>
2022-06-12 10:42:22 +0200

    s

--- Space Defender processi/AreaGioco.c
+++ Space Defender processi/AreaGioco.c
@@ -16,12 +16,6 @@ void area(int pipein,int nnemici){
 
     int i=0, j=0, k=0, vite=3, collision=0, alienimorti=0;
 
-    for(i=0;i<11;i++) A[i].alive=true;
-    for(i=0;i<11;i++) A[i].liv=1;
-    for(i=0;i<NMB;i++) BG[i].alive=true;
-    for(i=0;i<NMB;i++) BN[i].alive=true;
-    G.alive=true;
-
     
 
         do {
