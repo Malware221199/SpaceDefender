117cdc083fdd149cef4d43cc22bdabd34fc92c56
Malware221199 <samuele.starita@gmail.com>
2022-06-12 10:20:17 +0200

    s

--- Space Defender processi/AreaGioco.c
+++ Space Defender processi/AreaGioco.c
@@ -8,10 +8,10 @@
 
 void area(int pipein,int nnemici){
 
-    save alieni[11];
-    save bulletg[30];
-    save bulletn[30];
-    save giocatore;
+    save A[11];
+    save BG[30];
+    save BN[30];
+    save G;
     save dato_letto;
 
     int i=0, j=0, k=0, vite=3, collision=0, alienimorti=0;
@@ -28,25 +28,25 @@ void area(int pipein,int nnemici){
             if(dato_letto.c=='A') {
 
                 /* Cancello il precedente carattere visualizzato */
-                cancellasprite(alieni[dato_letto.id].y,alieni[dato_letto.id].x,dato_letto.c);
+                cancellasprite(A[dato_letto.id].y,A[dato_letto.id].x,dato_letto.c);
 
 
                 /* Aggiorno le coordinate relative alla nuova posizione */
-                alieni[dato_letto.id].id=dato_letto.id;
-                alieni[dato_letto.id].y=dato_letto.y;
-                alieni[dato_letto.id].x=dato_letto.x;
-                alieni[dato_letto.id].pid=dato_letto.pid;
+                A[dato_letto.id].id=dato_letto.id;
+                A[dato_letto.id].y=dato_letto.y;
+                A[dato_letto.id].x=dato_letto.x;
+                A[dato_letto.id].pid=dato_letto.pid;
 
                 /* Visualizzo il carattere dell'entità sulle nuove coordinate */
                 stampasprite(dato_letto.y,dato_letto.x,dato_letto.c);
 
                 /* Collisioni Alieni*/
-                if(alieni[dato_letto.id].x<10){
+                if(A[dato_letto.id].x<10){
                     vite--;
-                    kill(alieni[dato_letto.id].pid,1);
-                    cancellasprite(alieni[dato_letto.id].y,alieni[dato_letto.id].x,dato_letto.c);
-                    alieni[dato_letto.id].y=DEADYA;
-                    alieni[dato_letto.id].x=DEADXA;
+                    kill(A[dato_letto.id].pid,1);
+                    cancellasprite(A[dato_letto.id].y,A[dato_letto.id].x,dato_letto.c);
+                    A[dato_letto.id].y=DEADYA;
+                    A[dato_letto.id].x=DEADXA;
                     alienimorti++;
                     
                     
@@ -60,10 +60,10 @@ void area(int pipein,int nnemici){
             */
             else if(dato_letto.c=='G'){
                 /* Cancello il precedente sprite visualizzato */
-                cancellasprite(giocatore.y,giocatore.x,giocatore.c);
+                cancellasprite(G.y,G.x,G.c);
 
                 /* Aggiorno le coordinate relative alla nuova posizione */
-                giocatore=dato_letto;
+                G=dato_letto;
 
                 /* Visualizzo il carattere dell'entità sulle nuove coordinate */
                 stampasprite(dato_letto.y,dato_letto.x,dato_letto.c);
@@ -78,44 +78,46 @@ void area(int pipein,int nnemici){
             else if(dato_letto.c=='B'){
 
                 /* Cancello il precedente carattere visualizzato */
-                cancellasprite(bulletg[dato_letto.id].y,bulletg[dato_letto.id].x,dato_letto.c);
+                cancellasprite(BG[dato_letto.id].y,BG[dato_letto.id].x,dato_letto.c);
 
                 /* Aggiorno le coordinate relative alla nuova posizione */
-                bulletg[dato_letto.id].id=dato_letto.id;
-                bulletg[dato_letto.id].y=dato_letto.y;
-                bulletg[dato_letto.id].x=dato_letto.x;
-                bulletg[dato_letto.id].pid=dato_letto.pid;
+                BG[dato_letto.id].id=dato_letto.id;
+                BG[dato_letto.id].y=dato_letto.y;
+                BG[dato_letto.id].x=dato_letto.x;
+                BG[dato_letto.id].pid=dato_letto.pid;
 
                 /* Visualizzo il carattere dell'entità sulle nuove coordinate */
                 stampasprite(dato_letto.y,dato_letto.x,dato_letto.c);
             
                 
                 /* Collisioni proiettili con MAXX*/
-                if (bulletg[dato_letto.id].x>MAXX){
-                    kill(bulletg[dato_letto.id].pid,1);
-                    cancellasprite(bulletg[dato_letto.id].y,bulletg[dato_letto.id].x,'B');
-                    bulletg[dato_letto.id].x=DEADXB;
-                    bulletg[dato_letto.id].y=DEADYB;
+                if (BG[dato_letto.id].x>MAXX){
+                    kill(BG[dato_letto.id].pid,1);
+                    cancellasprite(BG[dato_letto.id].y,BG[dato_letto.id].x,'B');
+                    BG[dato_letto.id].x=DEADXB;
+                    BG[dato_letto.id].y=DEADYB;
                 }
                 /* Collisioni proiettili con alieni*/
-                int exit=0;
-                for(i=0;i<11;i++){
-                    for(j=0;j<GSA;j++){
-                        for(k=0;k<GSA;k++){
-                            if(bulletg[dato_letto.id].x == (alieni[i].x)+k && bulletg[dato_letto.id].y == (alieni[i].y)+j){
-                                kill(bulletg[dato_letto.id].pid,1);
-                                kill(alieni[alieni[i].id].pid,1);
-                                cancellasprite(alieni[i].y,alieni[i].x,'A');
-                                bulletg[dato_letto.id].y=DEADYB;
-                                bulletg[dato_letto.id].x=DEADXB;
-                                alieni[i].y=DEADYA;
-                                alieni[i].x=DEADXA;
-                                if(exit==0){
-                                    alienimorti++;
-                                    exit=1;
-                                }      
-                            }
+                 for(i=0;i<11;i++){
+                    if(A[i].alive){
+                       
+                        if(BG[dato_letto.id].alive && collisione(BG[j].cord, DB, DB,A[i].cord,GSA,GSA) && A[i].liv==1)
+                        {   
+                            
+                            BG[dato_letto.id].alive=false;
+                            kill(BG[dato_letto.id].pid,1);
+                            A[i].liv=2;
                         }
+                        else if(bulletg[dato_letto.id].alive && collisione(BG[j].cord, DB, DB,A[i].cord,GSA,GSA) && A[i].liv==2)
+                        {
+                            BG[dato_letto.id].alive=false;
+                            kill(BG[dato_letto.id].pid,1);
+                            A[i].alive=false;
+                            kill(A[i].pid,1);
+                            alienimorti++;
+                        }
+                            
+                        
                     }
                 }
             }
@@ -126,13 +128,13 @@ void area(int pipein,int nnemici){
             */
             else if(dato_letto.c=='H'){
                 /*Cancello il precedente carattere visualizzato */
-                cancellasprite(bulletn[dato_letto.id].y,bulletn[dato_letto.id].x,dato_letto.c);
+                cancellasprite(BN[dato_letto.id].y,BN[dato_letto.id].x,dato_letto.c);
                 
                 /*Aggiorno le coordinate relative alla nuova posizione */
-                bulletn[dato_letto.id].id=dato_letto.id;
-                bulletn[dato_letto.id].y=dato_letto.y;
-                bulletn[dato_letto.id].x=dato_letto.x;
-                bulletn[dato_letto.id].pid=dato_letto.pid;
+                BN[dato_letto.id].id=dato_letto.id;
+                BN[dato_letto.id].y=dato_letto.y;
+                BN[dato_letto.id].x=dato_letto.x;
+                BN[dato_letto.id].pid=dato_letto.pid;
 
                 /* Visualizzo il carattere dell'entità sulle nuove coordinate */
                 stampasprite(dato_letto.y,dato_letto.x,dato_letto.c);
@@ -170,14 +172,14 @@ void area(int pipein,int nnemici){
     } while(!collision);
 
     /* Termino i processi Alieni e Giocatore */	
-            kill(pidG,1);
-            for(i=0;i<11;i++) kill(alieni[i].pid,1);
-            for(i=0;i<30;i++) kill(bulletg[i].pid,1);
-            for(i=0;i<30;i++) kill(bulletn[i].pid,1);
+            kill(G.pid,1);
+            for(i=0;i<11;i++) kill(A[i].pid,1);
+            for(i=0;i<30;i++) kill(BG[i].pid,1);
+            for(i=0;i<30;i++) kill(BN[i].pid,1);
 
 }
-/*
-bool collisione(cord c1,int lar1, int lun1,cord c2,int lar2,int lun2)
+
+bool collisione(save c1,int lar1, int lun1,save c2,int lar2,int lun2)
 {
     return(
         c1.x < c2.x + lar2 &&
@@ -187,10 +189,10 @@ bool collisione(cord c1,int lar1, int lun1,cord c2,int lar2,int lun2)
     );
 }
 
-bool fuorischermo(cord c, int lar, int lun)
+bool fuorischermo(save c, int lar, int lun)
 {
     return(
         c.y + lun > MAXY || c.y < 0 || c.x + lar > MAXX || c.x < 0
     );
 
-}	*/	
+}		
