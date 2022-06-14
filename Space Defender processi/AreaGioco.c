354df17aa1408efa9abc1168f71c48b0b9cc4728
Malware221199 <samuele.starita@gmail.com>
2022-06-14 19:37:03 +0200

    s

--- Space Defender processi/AreaGioco.c
+++ Space Defender processi/AreaGioco.c
@@ -45,8 +45,8 @@ void area(int pipein,int nnemici){
             //A[dato_letto.id].alive=dato_letto.alive;
 
             /* Visualizzo il carattere dell'entità sulle nuove coordinate */
-            if(A[dato_letto.id].alive  && A[dato_letto.id].liv==1) stampasprite(dato_letto.y,dato_letto.x,'a');
-            else if(A[dato_letto.id].alive && A[dato_letto.id].liv==2) stampasprite(dato_letto.y,dato_letto.x,'A');
+            /*if(A[dato_letto.id].alive  && A[dato_letto.id].liv==1)*/ stampasprite(dato_letto.y,dato_letto.x,'a');
+            //else if(A[dato_letto.id].alive && A[dato_letto.id].liv==2) stampasprite(dato_letto.y,dato_letto.x,'A');
             
         }
 
