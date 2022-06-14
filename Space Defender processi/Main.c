6f5ed8050fd14dec5a47af2f5b0a857657ebec4f
Malware221199 <samuele.starita@gmail.com>
2022-06-14 15:05:28 +0200

    s

--- Space Defender processi/Main.c
+++ Space Defender processi/Main.c
@@ -67,7 +67,7 @@ int main(){
             area(p[0],nnemici);  /* invocazione funzione area di gioco */  
         
             /*Termino i processi*/
-            kill(pidG,1);
+            kill(G.pid,1);
             for(i=0;i<11;i++)   if(A[i].alive) kill(A[i].pid,1);
         }
     }while(!quit);
